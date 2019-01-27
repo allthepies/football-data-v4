b4372cdbc1fe29fa786c258e7001e333208097dd
Ludo Bonnet <lbonnet84@gmail.com>
2019-01-27 21:12:41 +0100

    adding missing endpoints and filters

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ class FootballData {
         });
     }
 
-    competitions({area} = {}) {
-        return this.instance.get(`/competitions?${querystring.stringify({areas: area})}`)
+    competitions({area, plan} = {}) {
+        return this.instance.get(`/competitions?${querystring.stringify({areas: area, plan})}`)
     }
 
     competition(competitionId) {
         return this.instance.get(`/competitions/${competitionId}`)
     }
 
-    teams(competitionId, {stage} = {}) {
-        return this.instance.get(`/competitions/${competitionId}/teams?${querystring.stringify({stage})}`)
+    teams(competitionId, {season, stage} = {}) {
+        return this.instance.get(`/competitions/${competitionId}/teams?${querystring.stringify({season, stage})}`)
     }
 
-    standings(competitionId) {
-        return this.instance.get(`/competitions/${competitionId}/standings`)
+    standings(competitionId, {standingType}) {
+        return this.instance.get(`/competitions/${competitionId}/standings?${querystring.stringify({standingType})}`)
     }
     
-    competitionMatches(competitionId, {dateFrom, dateTo, stage, status, matchday, group} = {}) {
-        return this.instance.get(`/competitions/${competitionId}/matches?${querystring.stringify({dateFrom, dateTo, stage, status, matchday, group})}`)
+    competitionMatches(competitionId, {dateFrom, dateTo, stage, status, matchday, group, season} = {}) {
+        return this.instance.get(`/competitions/${competitionId}/matches?${querystring.stringify({dateFrom, dateTo, stage, status, matchday, group, season})}`)
     }
 
     matches({competitions, dateFrom, dateTo, status} = {}) {
@@ -38,8 +38,8 @@ class FootballData {
         return this.instance.get(`/matches/${id}`)
     }
 
-    teamMatches(id,{dateFrom, dateTo, status, venue} = {}) {
-        return this.instance.get(`/teams/${id}/matches?${querystring.stringify({dateFrom, dateTo, status, venue})}`)
+    teamMatches(id,{dateFrom, dateTo, status, venue, limit} = {}) {
+        return this.instance.get(`/teams/${id}/matches?${querystring.stringify({dateFrom, dateTo, status, venue, limit})}`)
     }
 
     team(id) {
@@ -48,6 +48,14 @@ class FootballData {
 
     areas(id) {
         return this.instance.get(`/areas/${id}`)
+    }
+
+    player(id) {
+        return this.instance.get(`/player/${id}`)
+    }
+
+    playerMatches(id,{competitions, dateFrom, dateTo, status, limit} = {}) {
+        return this.instance.get(`/players/${id}/matches?${querystring.stringify({competitions, dateFrom, dateTo, status, limit})}`)
     }
 }
 
