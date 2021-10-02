f4f8738076c5b67d30f20cdee73bb13e523cc1d3
Andy Whitworth <andywhit@gmail.com>
2021-10-02 17:12:46 +0100

    Refactor

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const axios = require('axios');
-const querystring = require('querystring');
+const Agent = require('agentkeepalive');
+
+const httpKeepAliveAgent = new Agent({
+    maxSockets: 100,
+    maxFreeSockets: 10,
+    timeout: 60000, // active socket keepalive for 60 seconds
+    freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+});
+
+const httpsKeepAliveAgent = new Agent({
+    maxSockets: 100,
+    maxFreeSockets: 10,
+    timeout: 60000, // active socket keepalive for 60 seconds
+    freeSocketTimeout: 30000, // free socket keepalive for 30 seconds
+});
 
 class FootballData {
+
     constructor(token, timeout = 1000) {
         this.instance = axios.create({
             baseURL: 'http://api.football-data.org/v2/',
             timeout: timeout,
             headers: {
                 'X-Auth-Token': token
-            }
+            },
+            httpAgent: httpKeepAliveAgent,
+            httpsAgent: httpsKeepAliveAgent
         });
     }
 
@@ -16,7 +33,11 @@ class FootballData {
         area,
         plan
     } = {}) {
-        return this.instance.get(`/competitions?${querystring.stringify({areas: area, plan})}`)
+        const searchParams = new URLSearchParams({
+            areas: area,
+            plan
+        });
+        return this.instance.get(`/competitions?${searchParams.toString()}`)
     }
 
     competition(competitionId) {
@@ -27,13 +48,20 @@ class FootballData {
         season,
         stage
     } = {}) {
-        return this.instance.get(`/competitions/${competitionId}/teams?${querystring.stringify({season, stage})}`)
+        const searchParams = new URLSearchParams({
+            season,
+            stage
+        });
+        return this.instance.get(`/competitions/${competitionId}/teams?${searchParams.toString()}`)
     }
 
     standings(competitionId, {
         standingType
     } = {}) {
-        return this.instance.get(`/competitions/${competitionId}/standings?${querystring.stringify({standingType})}`)
+        const searchParams = new URLSearchParams({
+            standingType
+        });
+        return this.instance.get(`/competitions/${competitionId}/standings?${searchParams.toString()}`)
     }
 
     competitionMatches(competitionId, {
@@ -45,7 +73,16 @@ class FootballData {
         group,
         season
     } = {}) {
-        return this.instance.get(`/competitions/${competitionId}/matches?${querystring.stringify({dateFrom, dateTo, stage, status, matchday, group, season})}`)
+        const searchParams = new URLSearchParams({
+            dateFrom,
+            dateTo,
+            stage,
+            status,
+            matchday,
+            group,
+            season
+        });
+        return this.instance.get(`/competitions/${competitionId}/matches?${searchParams.toString()}`)
     }
 
     matches({
@@ -54,7 +91,13 @@ class FootballData {
         dateTo,
         status
     } = {}) {
-        return this.instance.get(`/matches?${querystring.stringify({competitions, dateFrom, dateTo, status})}`)
+        const searchParams = new URLSearchParams({
+            competitions,
+            dateFrom,
+            dateTo,
+            status
+        });
+        return this.instance.get(`/matches?${searchParams.toString()}`)
     }
 
     match(id) {
@@ -68,7 +111,14 @@ class FootballData {
         venue,
         limit
     } = {}) {
-        return this.instance.get(`/teams/${id}/matches?${querystring.stringify({dateFrom, dateTo, status, venue, limit})}`)
+        const searchParams = new URLSearchParams({
+            dateFrom,
+            dateTo,
+            status,
+            venue,
+            limit
+        });
+        return this.instance.get(`/teams/${id}/matches?${searchParams.toString()}`)
     }
 
     team(id) {
@@ -90,14 +140,25 @@ class FootballData {
         status,
         limit
     } = {}) {
-        return this.instance.get(`/players/${id}/matches?${querystring.stringify({competitions, dateFrom, dateTo, status, limit})}`)
+        const searchParams = new URLSearchParams({
+            competitions,
+            dateFrom,
+            dateTo,
+            status,
+            limit
+        });
+        return this.instance.get(`/players/${id}/matches?${searchParams.toString()}`)
     }
 
     scorers(competitionId, {
         limit,
         season
     } = {}) {
-        return this.instance.get(`/competitions/${competitionId}/scorers?${querystring.stringify({limit,season})}`)
+        const searchParams = new URLSearchParams({
+            limit,
+            season
+        });
+        return this.instance.get(`/competitions/${competitionId}/scorers?${searchParams.toString()}`)
     }
 
 
